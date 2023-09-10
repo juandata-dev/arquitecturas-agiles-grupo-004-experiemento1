aeb925099453c6c3a6ff88daaadecdfbe340a715
Juan David Torres <jd.torres2@uniandes.edu.co>
2023-09-09 19:27:19 -0500

    Feature: se agrega modelos para el microservicio motor de emparejamiento

---
+++ motor/modelos/modelos.py
@@ -0,0 +1,6 @@
+class candidato:
+    def __init__(self, name, email_address, title, phone_number=None):
+        self.name = name
+        self.email_address = email_address
+        self.title = title
+        self.phone_number = phone_number
